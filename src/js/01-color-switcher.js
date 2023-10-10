67875095880781400788df2bdb5575d46d4450e8
BestStepInUa <beststep.in.ua@gmail.com>
2023-10-10 13:03:05 +0300

    tast_1_done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(evt) {
+    evt.target.disabled = true;
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+function onStopBtnClick() {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+}
+// Random color generator function
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
