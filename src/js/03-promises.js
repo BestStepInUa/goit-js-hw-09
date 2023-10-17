66cfacc8bf7e32fafd2c556d952f4e93825df9cf
BestStepInUa <beststep.in.ua@gmail.com>
2023-10-17 13:44:08 +0300

    promises_fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,9 +2,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const Refs = {
   form: document.querySelector('.form'),
-  amount: document.getElementsByName('amount'),
-  delay: document.getElementsByName('delay'),
-  step: document.getElementsByName('step'),
   submitBtn: document.getElementsByTagName('button')
 }
 
@@ -13,24 +10,23 @@ Refs.form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  const formData = {
-    delay: Number(Refs.delay.value),
-    step: Number(Refs.step.value),
-    amount: Number(Refs.amount.vaslue)    
-  }
+  const formElems = evt.currentTarget.elements;
 
-  let counter = formData.delay;
+  let delay = Number(formElems.delay.value);
+  const step = Number(formElems.step.value);
+  const amount = Number(formElems.amount.value);  
+  
+  let delayCounter = delay;
 
-  for (let i = 1; i <= formData.amount; i++) {
-    createPromise(i, counter)
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delayCounter)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    counter += formData.step;
-    Refs.form.reset();
+    delayCounter += step;    
   }
 }
 
